f5f4db01d1c1ee324fe4bda6b63644ef207800c6
jrafaaael <jrmd070102@gmail.com>
2021-06-30 21:48:02 -0400

    basic mockup in template area

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <nav>
+      <!-- input here -->
+    </nav>
+    <section>
+      <!-- grid 2x2 here -->
+      <!-- row 1, column 1 and 2: general info (avatar, username, repos quantity) -->
+      <!-- row 2, column 1: repos -->
+      <!-- row 2, column 2: After clicking on a repository, generate a chart with highcharts of the languages ​​used in that repo -->
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  /* TODO: Find better fonts */
+  color: var(--primary-font-color);
+  font-family: sans-serif;
+  letter-spacing: 0.1rem;
+}
+
+:root {
+  --primary-color: #011638;
+  --secondary-color: #364156;
+
+  --primary-font-color: snow;
+}
+
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+
+body {
+  background-color: var(--primary-color);
 }
 </style>
