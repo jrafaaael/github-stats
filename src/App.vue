392f5b5e072dfe4d9f90ad53e122be7cc56afe09
jrafaaael <jrmd070102@gmail.com>
2021-07-01 15:51:56 -0400

    added navbar component to main view

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <main>
-    <nav>
-      <!-- input here -->
-    </nav>
+    <navbar />
     <section>
       <!-- grid 2x2 here -->
       <!-- row 1, column 1 and 2: general info (avatar, username, repos quantity) -->
       <!-- row 2, column 1: repos -->
       <!-- row 2, column 2: After clicking on a repository, generate a chart with highcharts of the languages ​​used in that repo -->
     </section>
+    <!-- This is to demonstrate the operation of the input and the different states that the project has:
+    1) "waiting": waiting for a valid username
+    2) "typing": state that occurs while the user types. Goes to the "fetching" state 1 second (1000 ms) after the last change in the input
+    3) "fetching": state while the GET request occurs -->
+    {{ STATE }}
+    <br />
+    <br />
+    {{ USER }}
+    <br />
+    <br />
+    {{ USER_INFO }}
   </main>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import Navbar from "./components/Navbar.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { Navbar },
+  computed: {
+    ...mapState(["STATE", "USER", "USER_INFO"]),
+  },
 };
 </script>
 
@@ -27,8 +42,8 @@ export default {
   padding: 0;
   box-sizing: border-box;
 
-  /* TODO: Find better fonts */
   color: var(--primary-font-color);
+  /* TODO: Find better fonts */
   font-family: sans-serif;
   letter-spacing: 0.1rem;
 }
@@ -36,6 +51,8 @@ export default {
 :root {
   --primary-color: #011638;
   --secondary-color: #364156;
+
+  --quaternary-color: #dff8eb;
 
   --primary-font-color: snow;
 }
