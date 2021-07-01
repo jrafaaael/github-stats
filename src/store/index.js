bed35ca0ecde7a9070e890aa6f6e9fae62286311
jrafaaael <jrmd070102@gmail.com>
2021-07-01 15:52:51 -0400

    added vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,37 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import FetchService from "../api/FetchService";
+
+const fs = new FetchService();
 
 export default createStore({
   state: {
+    STATE: "waiting",
+    USER: null,
+    USER_INFO: null,
   },
   mutations: {
+    setState(state, payload) {
+      state.STATE = payload;
+    },
+    setUser(state, payload) {
+      state.USER = payload;
+    },
+    setUserInfo(state, payload) {
+      state.USER_INFO = payload;
+    },
   },
   actions: {
+    changeState({ commit }, payload) {
+      commit("setState", payload);
+    },
+    changeUser({ commit }, payload) {
+      commit("setUser", payload);
+    },
+    async getUserInfo({ commit, state }) {
+      const userInfo = await fs.getUser(state.USER);
+      commit("setUserInfo", userInfo);
+      commit("setState", "waiting");
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
